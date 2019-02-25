bed5f8312733e1d49d49b5ae852aba247926e260
nevermindthelabel <nevermindthelabel@gmail.com>
2019-02-24 19:29:07 -0700

    removed morgan logger

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 // configure our middleware
-app.use(logger('dev'));
 
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
